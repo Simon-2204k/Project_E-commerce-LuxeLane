2ae58a9189381ce51de1e4966c9a12447a8949ef
Srinu <simon2204k@gmail.com>
2025-10-02 12:05:04 +0530

    just worked

--- src/components/home/HomeElectronics.jsx
+++ src/components/home/HomeElectronics.jsx
@@ -1,14 +1,13 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/new.png";
 const HomeElectronics = () => {
   return (
     <div>
-      <div className="fixed top-0 w-screen h-[10vh] overflow-hidden backdrop-blur-md flex justify-between items-center bg-white/20 border-b border-gray-300 px-10">
+      <div className="fixed top-0 w-screen h-[10vh] overflow-hidden backdrop-blur-lg flex justify-between items-center bg-white/20 border-b border-gray-300 px-10">
         <Link to="/home">
           <img
             src={logo}
-            className="h-[25vh] mt-3  -ml-9 object-contain" // logo taller than header
+            className="h-[25vh] mt-3  -ml-9 object-contain"
             alt="luxelane"
           />
         </Link>
@@ -20,7 +19,7 @@ const HomeElectronics = () => {
           ← Go to Home
         </Link>
       </div>
-      <h1 className="text-9xl ">electronic</h1>
+      <h1 className="text-9xl mt-20">electronic</h1>
     </div>
   );
 };
